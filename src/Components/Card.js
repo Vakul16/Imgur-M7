6f6889007320e8ac634ecd766d6d70a1cb8720bb
Vakul16 <valusavakhul@gmail.com>
2020-07-25 17:38:41 +0530

    Add

---
+++ src/Components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+export default class Genre extends Component {
+    render() {
+        // const {id, title, img, book, inWishlist} = this.props.genre;
+        return (
+            <div className = "col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className = "card">
+                <h1>Hello Imgur</h1>
+                </div>
+            </div>
+        )}
+}
