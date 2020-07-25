dde16639bc723e3e0cc59521b7bfba0a3be3ccd9
Vakul16 <valusavakhul@gmail.com>
2020-07-25 17:40:33 +0530

    empty

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 export default class Genre extends Component {
     render() {
-        // const {id, title, img, book, inWishlist} = this.props.genre;
         return (
             <div className = "col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className = "card">
